361ae4e5d1ed4ff9f3341b5da6d97569994170d3
TD-WEBSITE-0832 <abdullayvsaidxon76@gmail.com>
2025-12-27 12:25:44 +0500

    Hello

---
+++ script.js
@@ -0,0 +1,11 @@
+const search = document.querySelector(".search");
+
+search.addEventListener("focus", () => {
+    search.placeholder = "";
+});
+
+search.addEventListener("blur", () => {
+    if (search.value === "") {
+        search.placeholder = "Mahsulotlarni qidirish";
+    }
+});
