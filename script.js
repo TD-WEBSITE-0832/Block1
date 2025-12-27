2f9a753f0dd48e10a3fbfd486e8c8b302c0ba949
TD-WEBSITE-0832 <abdullayvsaidxon76@gmail.com>
2025-12-27 11:45:53 +0500

    New Projeck

--- script.js
+++
@@ -1,8 +0,0 @@
-const followBtn = document.getElementById('followBtn');
-const followCount = document.getElementById('followCount');
-
-followBtn.addEventListener('click', () => {
-    let count = Number(followCount.textContent);
-    count += 1;
-    followCount.textContent = count; 
-});
